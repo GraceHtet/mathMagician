f154af5bbc282741f6eaa8328d78074265f8a081
Thiri <thirihtet246@gmail.com>
2023-02-14 21:58:51 +0630

    Build Calculator
    Build Calculator design

---
+++ src/components/Calculator.js
@@ -0,0 +1,39 @@
+/* eslint-disable linebreak-style */
+import './Calculator.css';
+
+function Calculator() {
+  const numSigns = [
+    'AC',
+    '+/-',
+    '%',
+    '/',
+    7,
+    8,
+    9,
+    '*',
+    4,
+    5,
+    6,
+    '-',
+    1,
+    2,
+    3,
+    '+',
+    0,
+    '.',
+    '=',
+  ];
+
+  return (
+    <div className="cal-container">
+      <div className="display">0</div>
+      {numSigns.map((num) => (
+        <button type="button" className="btn" key={num}>
+          {num}
+        </button>
+      ))}
+    </div>
+  );
+}
+
+export default Calculator;
--- src/index.js
+++ src/index.js
@@ -5,8 +5,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 /* eslint-disable */
 import App from './App';
-/* eslint-disable */
-import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -14,8 +12,3 @@ root.render(
     <App />
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
