49e45c9da1551920559e21c635ca3b9612937e39
Thiri <thirihtet246@gmail.com>
2023-02-15 07:44:39 +0630

    Fix required changes
    Removed boilerplate
    Changed Function component to class component
    Fixed linter

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,39 +1,41 @@
-/* eslint-disable linebreak-style */
+import React from 'react';
 import './Calculator.css';
 
-function Calculator() {
-  const numSigns = [
-    'AC',
-    '+/-',
-    '%',
-    '/',
-    7,
-    8,
-    9,
-    '*',
-    4,
-    5,
-    6,
-    '-',
-    1,
-    2,
-    3,
-    '+',
-    0,
-    '.',
-    '=',
-  ];
+const numSigns = [
+  'AC',
+  '+/-',
+  '%',
+  '/',
+  7,
+  8,
+  9,
+  '*',
+  4,
+  5,
+  6,
+  '-',
+  1,
+  2,
+  3,
+  '+',
+  0,
+  '.',
+  '=',
+];
 
-  return (
-    <div className="cal-container">
-      <div className="display">0</div>
-      {numSigns.map((num) => (
-        <button type="button" className="btn" key={num}>
-          {num}
-        </button>
-      ))}
-    </div>
-  );
+class Calculator extends React.PureComponent {
+  render() {
+    return (
+      <div className="cal-container">
+        <div className="display">0</div>
+        {numSigns.map((num) => (
+          <button type="button" className="btn" key={num}>
+            {num}
+          </button>
+        ))}
+      </div>
+    );
+  }
 }
 
 export default Calculator;
--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
-/* eslint-disable */
 import React from 'react';
-/* eslint-disable */
 import ReactDOM from 'react-dom/client';
 import './index.css';
-/* eslint-disable */
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
 );
